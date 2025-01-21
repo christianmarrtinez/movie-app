4af13040db4e69ecc7c2a457bdadff1f7993eca6
christianmarrtinez <christianmarrtinez@gmail.com>
2025-01-20 20:30:09 -0500

    Add categories and favorites screen

--- App.js
+++ App.js
@@ -1,32 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import TabNavigator from './navigation/TabNavigator';
 
 const App = () => {
-  return (
-    <View style={styles.container}>
-      <Image source={require('./assets/icon.png')} style={styles.icon} />
-      <Text style={styles.text}>Welcome to your new Expo project!</Text>
-    </View>
-  );
+  return <TabNavigator />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  icon: {
-    width: 100,
-    height: 100,
-    marginBottom: 20,
-  },
-  text: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#333',
-  },
-});
 
 export default App;
---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CategoriesScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Categories Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#333',
+  },
+});
+
+export default CategoriesScreen;
